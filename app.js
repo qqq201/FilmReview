352dbc63deed6ab11de6a9b2faac06477d127c43
Pham Tien <19127576@student.hcmus.edu.vn>
2021-11-16 20:49:23 +0700

    Create a schema for username

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import mogan from 'morgan';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
 import {Router} from "express";
-import mongose from 'mongoose';
+import mongoose from 'mongoose';
 
 // Middleware
 const app = express();
@@ -16,10 +16,45 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const uri = 'mongodb+srv://reviewfilm:b7EMQHRsTd8XmM5@cluster0.e2shl.mongodb.net/FilmReview?retryWrites=true&w=majority'
 
-mongose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(uri).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.log('Connection failed', err);
 });
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    gender: String,
+    email: String,
+    city: String,
+    role: String
+});
+
+async function createPerson() {
+
+    const Person = mongoose.model('Person', personSchema);
+    const person = new Person({
+        name: 'Nguyễn Nguyễn',
+        email: 'Ng@gmail.com',
+        gender: 'Nam',
+        age: 23,
+        city: 'HCM city',
+        role: 'user'
+    });
+
+    const result = await person.save();
+    console.log(result);
+}
+
+async function getPerson() {
+
+    const Person = mongoose.model('Person', personSchema);
+    const person = await Person.find({name: 'Nguyễn Nguyễn'}).limit(1).sort({age: -1});
+    console.log(person);
+}
+
+getPerson();
 
 const port = process.env.PORT || 3000;
 
