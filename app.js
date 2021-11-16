d5c11578fa8bfa7d1114eb2582073c8172e02633
Pham Tien <19127576@student.hcmus.edu.vn>
2021-11-16 12:54:22 +0700

    Some minor
    Using npm start to run program

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url';
 import {Router} from "express";
 
 
+// Middleware
 const app = express();
 app.use(mogan('dev'));
 app.use(express.json());
@@ -14,10 +15,6 @@ app.use(express.static('public'));
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-    res.sendFile('/Users/lucky/FilmReview/index.html');
-});
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:3000`);
