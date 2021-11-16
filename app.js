7a0eae71135d9d2561c6e6356c8d9469451347ff
Pham Tien <19127576@student.hcmus.edu.vn>
2021-11-16 12:49:35 +0700

    Deploy a home page
    Push css file and images inside package public to serve easily with root directory

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import express from 'express';
 import mogan from 'morgan';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import {Router} from "express";
+
 
 const app = express();
 app.use(mogan('dev'));
 app.use(express.json());
+app.use(express.static('public'));
+
+// get the current file path of project
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile('/Users/lucky/FilmReview/index.html');
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:3000`);
 });
