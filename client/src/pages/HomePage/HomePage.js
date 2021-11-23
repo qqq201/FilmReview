57eefe903804241d51aff04706d18b347ad5dbb5
Quy Quach <quyquach201@gmail.com>
2021-11-23 17:48:58 +0700

    resolve conflicts

---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import Carousel from '../../components/carousel/carousel.js'
+import Gallery from '../../components/gallery/gallery.js'
+
+const Home = () => {
+    return (
+        <>
+            <Carousel/>
+            <Gallery name='Bộ sưu tập'/>
+        </>
+    )
+}
+
+export default Home;
