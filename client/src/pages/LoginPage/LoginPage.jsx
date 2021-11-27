2af2b3e9e52c606dda461b48c04c677c971789ba
Pham Tien <19127576@student.hcmus.edu.vn>
2021-11-28 01:34:53 +0700

    Add css for form Login

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,34 +1,50 @@
-import {Button, Container, Form, Row, Col} from 'react-bootstrap';
+import {Button, Col, Container, Form, Row} from 'react-bootstrap';
+import './loginPage..css'
 
 const LoginPage = () => {
     return (
-        <Container>
-            <Row>
-                <Col lg={6}>
-                    <Form>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email"/>
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password"/>
-                        </Form.Group>
-                        <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out"/>
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </Col>
-            </Row>
+        <div id="loginPage">
 
-        </Container>
+            <Container id="" className={"d-flex justify-content-end"}>
+                <Row>
+                    <Col lg={12} xl={12}>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email"/>
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password"/>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Check me out"/>
+                            </Form.Group>
+
+                            <Row>
+                                <Col>
+                                    <Button variant="btn btn-success btn-lg " type="submit">
+                                        Đăng Nhập
+                                    </Button>
+                                </Col>
+                                <Col>
+                                    <Button variant="btn btn-danger btn-lg" type="submit">
+                                        Huỷ
+                                    </Button>
+                                </Col>
+                            </Row>
+
+                        </Form>
+                    </Col>
+                </Row>
+
+            </Container>
+        </div>
     )
 }
 
